177140cf30db16e53526e950e9f2ef498d153cfa
Niladri Das <bniladridas@users.noreply.github.com>
2026-02-13 04:31:01 +0530

    chore: add harper homebrew formula

---
+++ Formula/harper.rb
@@ -0,0 +1,28 @@
+class Harper < Formula
+  desc "AI for the terminal - translates natural language into reviewed, executable commands"
+  homepage "https://github.com/harpertoken/harper"
+  version "0.4.0"
+  license "Apache-2.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/harpertoken/harper/archive/refs/tags/0.4.0.tar.gz"
+      sha256 "b5ecf8227c2fb6510aa51d2c6454d80b8bad075e571456131dac1007a38bfef9"
+    else
+      url "https://github.com/harpertoken/harper/archive/refs/tags/0.4.0.tar.gz"
+      sha256 "b5ecf8227c2fb6510aa51d2c6454d80b8bad075e571456131dac1007a38bfef9"
+    end
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release", "--locked"
+    bin.install "target/release/harper" => "harper"
+    bin.install "target/release/harper-batch" => "harper-batch" if File.exist?("target/release/harper-batch")
+  end
+
+  test do
+    assert_match "Harper", shell_output("#{bin}/harper --version")
+  end
+end
